21cb43b8dbf1fa4bba73076179f1aee21b65f675
William_Twomey <48021997+Reilig35@users.noreply.github.com>
2021-08-22 19:58:06 +0100

    Add project files.

---
+++ Generuics_Assignment/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Generuics_Assignment
+{
+    public class Employee<T>
+    {
+        public string fName { get; set; }
+        public string lName { get; set; }
+        public int Id { get; set; }
+        //Creating a List that uses generic type Employee when initilized
+        public List<T> Things { get; set; }
+    }
+}
+
+
---
+++ Generuics_Assignment/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Generuics_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Initanilsing generic employee with string data type
+            Employee<string> employee = new Employee<string>();
+            employee.Things = new List<string>() { "William", "John", "Tony", "Dave"};
+
+            foreach (string i in employee.Things)
+            {
+                Console.WriteLine("{0}" , i);
+            }
+
+            //Initanilsing generic employee with int data type
+            Employee<int> employer = new Employee<int>();
+            employer.Things = new List<int>() { 10,9,8,7,6,5,4,3,2,1,0};
+
+            foreach (int i in employer.Things)
+            {
+                Console.WriteLine("{0}" , i);
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
